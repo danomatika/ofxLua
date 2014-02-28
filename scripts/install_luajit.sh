24eec22dd612d546dc2d5d2c28e081217b674a06
abigpotostew <stew.bracken@gmail.com>
2014-02-28 11:24:26 -0800

    luajit working, but working on linking it with local lib instead of installing luajit systemwide

--- scripts/install_luajit.sh
+++ scripts/install_luajit.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
+LUAJIT=luajit-2.0
+
+### You must cd into the scripts folder for this to work! #############
+
 #need absolute path for proper jit installation
 CUR_DIR=$(pwd)
 JIT_DEST=../libs/luajit
 INSTALL_DIR=$CUR_DIR/$JIT_DEST
 
-#download jitlua
+# Download jitlua
 git clone http://luajit.org/git/luajit-2.0.git
 
 # Create folder for installation files
 mkdir -p $JIT_DEST
 
-
 ### BUILD LuaJIT-2.0 #################
-cd luajit-2.0
-# Build a 32-bit binary even if native machine is 64 bit, OF is 32.
+cd $(LUAJIT)-2.0
+# Build a 32-bit binary even if native machine is 64 bit, OF is 32 only.
 make CC="gcc -m32"
 
 ### INSTALL LuaJIT-2.0 ###############
@@ -22,9 +25,9 @@ make install PREFIX=$INSTALL_DIR
 mv COPYRIGHT $INSTALL_DIR/.
 mv README $INSTALL_DIR/.
 
-### Clean build files ##############
+### Clean build files ###############
 cd ../
-rm -R luajit-2.0
+rm -R $(LUAJIT)
 
 ### Clean install files ##############
 rm -R $JIT_DEST/bin
