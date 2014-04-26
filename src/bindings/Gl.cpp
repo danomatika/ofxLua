5474bde366ac750aedd356fadee56fdfb2275326
abigpotostew <stew.bracken@gmail.com>
2014-04-26 10:06:40 -0700

    whitespace changes

--- src/bindings/Gl.cpp
+++ src/bindings/Gl.cpp
@@ -183,24 +183,25 @@ luabind::scope registerGl() {
 					.def_readwrite("wrapModeHorizontal", &ofFbo::Settings::wrapModeHorizontal)
 					.def_readwrite("minFilter", &ofFbo::Settings::minFilter)
 					.def_readwrite("maxFilter", &ofFbo::Settings::maxFilter)
-					.def_readwrite("numSamples", &ofFbo::Settings::numSamples)
-                
+					.def_readwrite("numSamples", &ofFbo::Settings::numSamples),
                     
+                    //Use of.Fbo.INTERNAL_FORMAT.GL_RGBA 
+                    class_<InternalFormat>("INTERNAL_FORMAT")
+                    .enum_("mode") [
+                                    value("GL_RGBA", GL_RGBA),
+                                    value("GL_RGBA16", GL_RGBA16),
+                                    value("GL_RGBA32F", GL_RGBA32F),
+                                    value("GL_RGBA16F_ARB", GL_RGBA16F_ARB),
+                                    value("GL_RGBA32F_ARB", GL_RGBA32F_ARB),
+                                    value("GL_LUMINANCE32F_ARB", GL_LUMINANCE32F_ARB),
+                                    value("GL_LUMINANCE16", GL_LUMINANCE16),
+                                    value("GL_RGB", GL_RGB),
+                                    value("GL_RGB16", GL_RGB16),
+                                    value("GL_RGB32F", GL_RGB32F)
+                                    ]
 			],
     
-        class_<InternalFormat>("INTERNAL_FORMAT")
-        .enum_("mode") [
-                    value("GL_RGBA", GL_RGBA),
-                    value("GL_RGBA16", GL_RGBA16),
-                    value("GL_RGBA32F", GL_RGBA32F),
-                    value("GL_RGBA16F_ARB", GL_RGBA16F_ARB),
-                    value("GL_RGBA32F_ARB", GL_RGBA32F_ARB),
-                    value("GL_LUMINANCE32F_ARB", GL_LUMINANCE32F_ARB),
-                    value("GL_LUMINANCE16", GL_LUMINANCE16),
-                    value("GL_RGB", GL_RGB),
-                    value("GL_RGB16", GL_RGB16),
-                    value("GL_RGB32F", GL_RGB32F)
-                    ],
+    
 			
 		///////////////////////////////
 		/// \section of3dUtils.h
