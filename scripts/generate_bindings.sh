d822a6d8aef28b675548d542bec60d505d950a7d
danomatika <danomatika@gmail.com>
2023-10-29 20:25:50 +0100

    all for generating a single specific target when running generate_bindings.sh

--- scripts/generate_bindings.sh
+++ scripts/generate_bindings.sh
@@ -8,12 +8,17 @@ set -e
 
 SWIG_FLAGS="DEST_DIR=../src/bindings"
 
+# all targets, specify a single target as $1
+TARGETS="desktop ios linuxarm header libs"
+if [[ "$1" != "" && $(echo $TARGETS | grep -w $1) && true ]] ; then
+	TARGETS=$1
+	shift
+fi
+
 ###
 
 cd $(dirname $0)/../swig
 
-make desktop $SWIG_FLAGS $@
-make ios $SWIG_FLAGS $@
-make linuxarm $SWIG_FLAGS $@
-make header $SWIG_FLAGS $@
-make libs $SWIG_FLAGS $@
+for target in $TARGETS ; do
+	make $target $SWIG_FLAGS $@
+done
