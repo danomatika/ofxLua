0023c12f98b932a998d844b708f33a4554eae715
danomatika <danomatika@gmail.com>
2025-01-20 12:46:22 +0100

    add addon_config macos target

--- addon_config.mk
+++ addon_config.mk
@@ -114,7 +114,9 @@ android/armeabi-v7a:
 	ADDON_SOURCES_EXCLUDE += src/bindings/desktop/% src/bindings/linuxarm/%
 	ADDON_INCLUDES_EXCLUDE += src/bindings/desktop/% src/bindings/linuxarm/%
 
-osx:
+osx: macos
+
+macos:
 	ADDON_CFLAGS += -DLUA_USE_MACOSX
 	ADDON_SOURCES_EXCLUDE += src/bindings/ios/% src/bindings/linuxarm/%
 	ADDON_INCLUDES_EXCLUDE += src/bindings/ios/% src/bindings/linuxarm/%
